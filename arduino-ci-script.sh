d6d1594f2ce3f5f2034a17d8acd12020f1826e82
per1234 <accounts@perglass.com>
2017-06-18 01:39:00 -0700

    Echo warning to log if commenting the report link fails

--- arduino-ci-script.sh
+++ arduino-ci-script.sh
@@ -1081,6 +1081,9 @@ function comment_report_link()
   local -r reportURL="$2"
 
   eval curl $ARDUINO_CI_SCRIPT_VERBOSITY_OPTION --header "\"Authorization: token ${token}\"" --data \"{'\"'body'\"':'\"'Once completed, the job reports for Travis CI [build ${TRAVIS_BUILD_NUMBER}]\(https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}\) will be found at:\\n${reportURL}'\"'}\" "\"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/commits/${TRAVIS_COMMIT}/comments\"" "$ARDUINO_CI_SCRIPT_VERBOSITY_REDIRECT"
+  if [[ $? -ne $ARDUINO_CI_SCRIPT_SUCCESS_EXIT_STATUS ]]; then
+    echo "ERROR: Failed to comment link to published report location"
+  fi
 }
 
 
