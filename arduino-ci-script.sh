3efe435924a6782b9c157dd37552e29bb91f3d78
per1234 <accounts@perglass.com>
2017-07-15 10:04:23 -0700

    Echo to log on each IDE version installed
    If the download/installation process is going slowly when installing a lot of IDE versions this function may cause the build to fail due to exceeding Travis CI's 10 minutes without log output timeout so it's necessary to periodically print something.

--- arduino-ci-script.sh
+++ arduino-ci-script.sh
@@ -236,6 +236,8 @@ function install_ide()
   eval "$INSTALLED_IDE_VERSION_LIST_ARRAY"
   local IDEversion
   for IDEversion in "${IDEversionListArray[@]}"; do
+    # If the download/installation process is going slowly when installing a lot of IDE versions this function may cause the build to fail due to exceeding Travis CI's 10 minutes without log output timeout so it's necessary to periodically print something.
+    echo "Installing: $IDEversion"
     # Determine download file extension
     local tgzExtensionVersionsRegex="1.5.[0-9]"
     if [[ "$IDEversion" =~ $tgzExtensionVersionsRegex ]]; then
